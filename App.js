963ac7e6e6ea361a2cf89aafe18a40c06be0af8e
luskas8 <gnomo1049@gmail.com>
2023-11-01 11:10:41 -0300

    format: lint

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
-//import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
 
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 
-import HomePage  from './src/pages/home'
-import MapPage  from './src/pages/map'
-import TutorialPage  from './src/pages/tutorial'
+import HomePage from './src/pages/home'
+import MapPage from './src/pages/map'
+import TutorialPage from './src/pages/tutorial'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
-export function PagesStack() {
-    return (
-        <Stack.Navigator screenOptions={{headerShown: false}}>
+export function PagesStack () {
+  return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Home" component={HomePage} />
             <Stack.Screen name="Mapa" component={MapPage} />
             <Stack.Screen name="Tutorial" component={TutorialPage} />
         </Stack.Navigator>
-    );
+  )
 }
 
-export default function App() {
+export default function App () {
   return (
     <NavigationContainer>
       <PagesStack/>
     </NavigationContainer>
-  );
+  )
 }
