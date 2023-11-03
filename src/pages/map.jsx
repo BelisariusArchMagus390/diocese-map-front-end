29989d09d729659d915fa3455bd1f8264e0c6bb8
luskas8 <gnomo1049@gmail.com>
2023-11-02 22:07:42 -0300

    feat: eslint config, back to js files

--- src/pages/map.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-
-export default function Map () {
-  return (
-        <View>
-            <Text>Map</Text>
-        </View>
-  )
-}
--- src/pages/tutorial.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-
-export default function Tutorial () {
-  return (
-        <View>
-            <Text>Tutoras</Text>
-        </View>
-  )
-}
